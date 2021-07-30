d642f46f3a1881863922fe33e242f69b9e72d953
Ben Butt <bgb25@cam.ac.uk>
2021-07-30 18:25:04 +0100

    Minor reworks to result_tools

--- atomic_virology.py
+++ atomic_virology.py
@@ -5,19 +5,20 @@ import result_tools
 
 """
 ### SEQ_TOOLS TESTING ###
-
 sars_cov_2 = seq_tools.fasta("./test_data/sequence.fasta")
 orfs = sars_cov_2.find_orfs(100)
-sars_cov_2.write_orf_fastas("./test_data")
+sars_cov_2.write_orf_fastas("./test_data/ORFs")
 """
 
 """
 ### RESULT_TOOLS TESTING ###
-test = result_tools.results("test_data/results")
-test.plot_plddts()
 """
+test = result_tools.result("./test_data/ul51")
+test.get_results()
+test.get_plddts()
+test.plot_plddts()
 
 """
 ### MODEL_TOOLS TESTING ###
+ranked = model_tools.models("test_data/models)
 """
-ranked = model_tools.models("test_data/models")
--- result_tools.py
+++ result_tools.py
@@ -1,12 +1,14 @@
 import pandas as pd
-
-
 from matplotlib import pyplot as plt
 
-class results:
+class result:
     def __init__(self, path):
         self.path = path
+
+    def get_results(self):
         self.results = [ pd.read_pickle(f"{self.path}/result_model_{i}.pkl") for i in range(1,6) ]
+
+    def get_plddts(self):
         self.plddts = [ result["plddt"] for result in self.results ]
 
     def plot_plddts(self):
