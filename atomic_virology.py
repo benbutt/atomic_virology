5733d50b9a99a2b044f400e2ea7d5103b9021274
Ben Butt <bgb25@cam.ac.uk>
2021-08-05 10:42:17 +0100

    Removed references to model_tools.py (deprecated)

--- atomic_virology.py
+++ atomic_virology.py
@@ -1,25 +1,17 @@
 import seq_tools
 import result_tools
 
-"""
+
 ### SEQ_TOOLS TESTING ###
-"""
 sars_cov_2 = seq_tools.fasta("./test_data/sequence.fasta")
 orfs = sars_cov_2.find_orfs(100)
 sars_cov_2.write_orf_fastas("./test_data/ORFs")
 
-
-"""
 ### RESULT_TOOLS TESTING ###
 
-test = result_tools.result("./test_data/ul51")
-test.get_results()
-test.get_plddts()
-test.plot_plddts()
-test.get_models()
-test.write_bfactors()
-"""
-"""
-### MODEL_TOOLS TESTING ###
-ranked = model_tools.models("test_data/models)
-"""
+# test = result_tools.result("./test_data/ul51")
+# test.get_results()
+# test.get_plddts()
+# test.plot_plddts()
+# test.get_models()
+# test.write_bfactors()
