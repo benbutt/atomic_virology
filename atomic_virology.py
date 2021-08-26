93c39c90617f139d629fff380faf23be8d8a9914
Ben Butt <bgb25@cam.ac.uk>
2021-08-26 13:20:27 +0100

    Added rudimentary argument parsing

--- atomic_virology.py
+++ atomic_virology.py
@@ -6,13 +6,33 @@ import result_tools
 # orfs = sars_cov_2.find_orfs(min_length=100)
 # sars_cov_2.write_orf_fastas()
 
-### RESULT_TOOLS TESTING ###
-test = result_tools.result("./test_data/ul51")
-test.get_results()
-test.get_plddts()
-test.plot_plddts()
-test.get_models()
-test.write_bfactors()
-test.get_msas()
-test.calculate_msa_depths()
-test.plot_msa_depth()
+# ### RESULT_TOOLS TESTING ###
+# test = result_tools.result("./test_data/ul51")
+# test.get_results()
+# test.get_plddts()
+# test.plot_plddts()
+# test.get_models()
+# test.write_bfactors()
+# test.get_msas()
+# test.calculate_msa_depths()
+# test.plot_msa_depth()
+
+def main():
+    alphafold_result = result_tools.result(args.result_dir)
+    alphafold_result.get_results()
+    alphafold_result.get_plddts()
+    alphafold_result.plot_plddts()
+    alphafold_result.get_models()
+    alphafold_result.write_bfactors()
+    alphafold_result.get_msas()
+    alphafold_result.calculate_msa_depths()
+    alphafold_result.plot_msa_depth()
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description="Process raw AlphaFold2 output and build Jupyter notebook containing results")
+    parser.add_argument("result_dir", type=str, help="Top-level directory containing AF2 output (usually target name)")
+    args = parser.parse_args()
+
+    main()
