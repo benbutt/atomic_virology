5793e39d00a9c9c3673ac7807e115fb6dd2c1255
Ben Butt <bgb25@cam.ac.uk>
2022-01-12 15:38:58 +0000

    Current working version for VPS4:UL71 models

--- atomic_virology.py
+++ atomic_virology.py
@@ -1,32 +1,24 @@
-import seq_tools
 import result_tools
 
-# ## SEQ_TOOLS TESTING ###
-# sars_cov_2 = seq_tools.fasta("./test_data/sequence.fasta")
-# orfs = sars_cov_2.find_orfs(min_length=100)
-# sars_cov_2.write_orf_fastas()
-
-# ### RESULT_TOOLS TESTING ###
-# test = result_tools.result("./test_data/ul51")
-# test.get_results()
-# test.get_plddts()
-# test.plot_plddts()
-# test.get_models()
-# test.write_bfactors()
-# test.get_msas()
-# test.calculate_msa_depths()
-# test.plot_msa_depth()
-
 def main():
+    # Parse input dir
     alphafold_result = result_tools.result(args.result_dir)
-    alphafold_result.get_results()
+    # Parse and tidy up results
+    alphafold_result.get_results() # Should I put this in init?
+    # Extract and plot pAE scores
+    alphafold_result.get_paes()
+    alphafold_result.plot_paes()
+    # Extract and plot pLDDT scores
     alphafold_result.get_plddts()
     alphafold_result.plot_plddts()
+    # Get models and superimpose them on best model
     alphafold_result.get_models()
-    alphafold_result.write_bfactors()
-    alphafold_result.get_msas()
-    alphafold_result.calculate_msa_depths()
-    alphafold_result.plot_msa_depth()
+    alphafold_result.superimpose_models()
+
+    # TODO: MSA handling for multimers
+    # # alphafold_result.get_msas()
+    # # alphafold_result.calculate_msa_depths()
+    # # alphafold_result.plot_msa_depth()
 
 
 if __name__ == "__main__":
--- result_tools.py
+++ result_tools.py
@@ -3,10 +3,12 @@ import subprocess
 import shutil
 from typing import List, Dict
 from glob import glob
+
 import pandas as pd
 from matplotlib import pyplot as plt
+import matplotlib.ticker as ticker
 from Bio.PDB.Structure import Structure
-from Bio.PDB import PDBParser
+from Bio.PDB import PDBParser, Superimposer
 from Bio.PDB.PDBIO import PDBIO
 from Bio import AlignIO
 from Bio.Align import MultipleSeqAlignment
@@ -20,7 +22,6 @@ class result:
         """
         Initialises result class with path to AF result directory
         """
-        print(path)
         self.path = os.path.realpath(path)
         print(f"Found result directory at {self.path}/")
 
@@ -38,7 +39,7 @@ class result:
         print(f"Raw AlphaFold output moved to {raw_dir}/")
 
         ## Extract result pickle file for each prediction, parse and return
-        result_paths = ( os.path.join(self.path, f"raw_output/result_model_{i}.pkl") for i in range(1,6) ) # Each path is generated on demand
+        result_paths = sorted(glob(f"{self.path}/raw_output/result_model_*.pkl"))
         self.results = [ pd.read_pickle(result_path) for result_path in result_paths ] # Parse each results pickle using Pandas
         print(f"Parsed {len(self.results)} results files")
         return self.results # Return full contents of results pickle as a list of dictionaries
@@ -50,7 +51,7 @@ class result:
         """
         ## Extract the pLDDT entry from each results dictionary
         self.plddts = [ result["plddt"] for result in self.results ]
-        print(f"Extracted pLDDT scores from {len(self.results)} results files")
+        print(f"Extracted pLDDT scores from {len(self.plddts)} results files")
         return self.plddts # Return pLDDT scores as a list of NumPy arrays
 
     def plot_plddts(self) -> None:
@@ -67,8 +68,8 @@ class result:
 
         ## Do the plotting
         for i, plddts in enumerate(self.plddts): # Iterate through the pLDDT arrays
-            plt.plot(range(1, len(plddts,)+1), plddts, label=f"Ranked_{i}.pdb", zorder=0-i) # Plot each array against residue number, decreasing z-order for each plot (i.e. ranked_0 on top)
-        
+            ax.plot(range(1, len(plddts,)+1), plddts, label=f"Ranked_{i}.pdb", zorder=0-i) # Plot each array against residue number, decreasing z-order for each plot (i.e. ranked_0 on top)
+
         ## Tidy up the plot
         ax.set_xlabel("Residue number")
         ax.set_ylabel("pLDDT")
@@ -81,40 +82,92 @@ class result:
         plt.savefig(f"{plots_dir}/pLDDTs.svg")
         print(f"Per-residue pLDDT plot saved to {plots_dir}/pLDDTs")
 
+        # TODO: If multimer, mark chain termini on plot
+
+    def get_paes(self) -> List[np.ndarray]:
+        """
+        Extracts, stores and returns predicted aligned error scores from results pickle
+        Requires: get_results()
+        """
+        ## Extract pAE scores from each results dictionary
+        self.paes = [ result["predicted_aligned_error"] for result in self.results ]
+
+        print(f"Extracted pAE scores from {len(self.paes)} results files")
+        return self.paes # Return pAE scores as a list of NumPy arrays
+
+    def plot_paes(self) -> None:
+        """
+        Plots per-residue pLDDT scores
+        Requires: get_results(), get_plddts()
+        """
+        ## Make a new subdirectory for plots if it doesn't already exist
+        plots_dir = os.path.join(self.path, "plots")
+        os.makedirs(plots_dir, exist_ok=True)
+
+        ## Initialise the plot object
+        fig, axs = plt.subplots(nrows=1, ncols=5, sharey=True, constrained_layout=True)
+
+        for i, pae in enumerate(self.paes):
+            axs[i].imshow(pae)
+            axs[i].set_title(f"Model {i+1}")
+
+            # TODO: Wrap up in function
+            axs[i].xaxis.set_major_locator(ticker.MultipleLocator(50))
+            axs[i].xaxis.set_minor_locator(ticker.MultipleLocator(10))
+            
+            axs[i].yaxis.set_major_locator(ticker.MultipleLocator(50))
+            axs[i].yaxis.set_minor_locator(ticker.MultipleLocator(10))
+            
+        ## Save the plot
+        plt.savefig(f"{plots_dir}/pAEs.png", dpi=600)
+        plt.savefig(f"{plots_dir}/pAEs.svg")
+        print(f"Predicted aligned error plots saved to {plots_dir}/pAEs")
+
     def get_models(self) -> List[Structure]:
         """
         Parses, stores and returns ranked models from results directory
         Requires: get_results()
         """
+
+        #TODO: Superimpose models
+
         parser = PDBParser() # Initialise a PDB parser object
         model_paths = ( os.path.join(self.path, f"raw_output/ranked_{i}.pdb") for i in range(5) ) # Generate the model paths on demand
         self.models = [ parser.get_structure(f"ranked_{i}", model_path) for i, model_path in enumerate(model_paths) ] # Parse each model and store as a list of models
         print(f"Parsed {len(self.models)} models")
         return self.models # Return a list of models
 
-    def write_bfactors(self) -> None:
+
+    def superimpose_models(self) -> None:
         """
-        Writes out new PDB files with per-residue pLDDT scores in the B factor column
-        Requires: get_results(), get_plddts(), get_models()
+        Superimposes all models on best model (ranked_0)
+        Requires: get_models()
         """
-        ## Set the b factor column for all atoms in each residue from the relevant pLDDT array
-        for i, model in enumerate(self.models): # Iterate through the models
-            for j, residue in enumerate(model.get_residues()): # Iterate through the residues from each model
-                for atom in residue.get_atoms(): # Iterate through the atoms from each residue
-                    atom.bfactor = self.plddts[i][j] # Set the B factor of each atom using the pLDDT array from get_plddts()
-        print(f"Wrote pLDDT scores to B factor column of {len(self.models)} models")
-        ## Make a new subdirectory for storing model outputs
-        models_dir = os.path.join(self.path, "models")
+        ## Make a new subdirectory for aligned models if it doesn't already exist
+        models_dir = os.path.join(self.path, "aligned_models")
         os.makedirs(models_dir, exist_ok=True)
 
-        # TODO: Superimpose all models on ranked_0 before writing out for easier comparison
-        
-        ## Write out the new models with pLDDT score in B factor column
-        io = PDBIO() # Initialise a PDBIO object for writing PDB files
-        for i, model in enumerate(self.models): # Iterate through the models
-            io.set_structure(model) # Get the current model
-            io.save(f"{models_dir}/ranked_{i}_plddts.pdb") # Write each model in models subdirectory
-        print(f"Saved {len(self.models)} models with pLDDT scores to {models_dir}")
+        sup = Superimposer()
+        io = PDBIO()
+
+        ref_model = self.models[0]
+
+        ref_CAs = [ atom for atom in ref_model.get_atoms() if atom.id == "CA" ]
+
+        for i, test_model in enumerate(self.models[1:]):
+            test_CAs = [ atom for atom in test_model.get_atoms() if atom.id == "CA" ]
+            sup.set_atoms(ref_CAs, test_CAs)
+            print(f"Superimosed model {i+2}, RMSD={sup.rms:.2f}")
+            sup.apply(test_model)
+
+            model_path = os.path.join(models_dir, f"ranked_{i+1}_aligned.pdb")
+            io.set_structure(test_model)
+            io.save(model_path)
+            
+        io.set_structure(ref_model)
+        model_path = os.path.join(models_dir, "ranked_0_aligned.pdb")
+        io.save(model_path)
+
 
     def convert_a3m(self, infile, outfile) -> None:
         """
@@ -133,6 +186,9 @@ class result:
         Requires "reformat.pl" conversion script from hhsuite to convert a3m format
         Requires: get_results()
         """
+
+        #TODO: If multimer, parse chain MSAs seperately
+
         ## Locate MSA directory
         msa_dir = os.path.join(self.path, "raw_output/msas/")
 
